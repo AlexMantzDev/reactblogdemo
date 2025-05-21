e729dd538bf17188170c2c743a29a8b2ba2d6bb0
Alex Mantz <alex.mantz@yahoo.com>
2025-05-21 15:02:40 -0500

    added feature to be able to submit posts.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,41 @@ import { useState } from "react";
 import "./App.css";
 import Post from "./components/Post";
 import { useEffect } from "react";
+import PostForm from "./components/PostForm";
 
 function App() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    const getPosts = async () => {
-      const res = await fetch("/api/v1/posts/");
-      const data = await res.json();
-      console.log(data);
-      setPosts(data);
-    };
-
     getPosts();
   }, []);
 
+  async function getPosts() {
+    const res = await fetch("/api/v1/posts/");
+    const data = await res.json();
+    setPosts(data);
+  }
+
+  async function handleSubmit(post) {
+    try {
+      const res = await fetch("/api/v1/posts/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(post),
+      });
+      await res.json();
+      //fetch updated posts from server
+      await getPosts();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div>
+      <PostForm onSubmit={handleSubmit}></PostForm>
       {posts.map((post) => {
         return (
           <Post key={post._id} title={post.title} content={post.content}></Post>
